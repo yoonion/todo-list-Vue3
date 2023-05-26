7f40cfa9400d73ab9ec34e8484d9468b3c35660e
KSY <yoonion97@gmail.com>
2023-05-26 17:43:53 +0900

    Feat: 기본 폼 작성 및 할일 추가 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>To-Do List</h1>
+      <form class="d-flex" @submit.prevent="onSubmit">
+        <div class="flex-grow-1 mr-2">
+          <input class="form-control" type="text" v-model="todo" placeholder="Write new task">
+        </div>
+        <div>
+          <button class="btn btn-primary" type="submit">Add</button>  
+        </div>
+      </form>
+
+      <div v-for="todo in todoList" :key="todo.id" class="card mt-2">
+        <div class="card-body p-2">
+          {{ todo.subject }}
+        </div>
+      </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const todo = ref('');
+    const todoList = ref([]);
+
+    const onSubmit = () => {
+      // console.log('onSubmit !!');
+
+      // add task 
+      todoList.value.push({
+        id: Date.now(),
+        subject: todo.value
+      })
+    };
+
+    return {
+      todo,
+      todoList,
+      onSubmit
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
