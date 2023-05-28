c55bb0fcac745a88a8219e5d72373b5a8ad2d621
KSY <yoonion97@gmail.com>
2023-05-28 15:58:02 +0900

    Feat: 할 일 삭제 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="container">
+
     <h1>To-Do List</h1>
-      <form class="d-flex" @submit.prevent="onSubmit">
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
         <div class="flex-grow-1 mr-2">
           <input class="form-control" type="text" v-model="todo" placeholder="Write new task">
         </div>
         <div>
           <button class="btn btn-primary" type="submit">Add</button>  
         </div>
-      </form>
-
-      <div v-for="todo in todoList" :key="todo.id" class="card mt-2">
-        <div class="card-body p-2">
-          {{ todo.subject }}
-        </div>
       </div>
+      <div v-show="hasErr" style="color: red">This field cannot be empty</div>
+    </form>
+
+    <div v-for="(todo, index) in todoList" :key="todo.id" class="card mt-2">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
+          <input v-model="todo.completed" class="form-check-input" type="checkbox">
+          <label :class="{ todoCompleted: todo.completed }" class="form-check-label">{{ todo.subject }}</label>
+        </div>
+        <div>
+            <button class="btn btn-danger btn-sm" @click="deleteTask(index)">Delete</button>
+          </div>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -26,26 +36,41 @@ export default {
   setup() {
     const todo = ref('');
     const todoList = ref([]);
+    const hasErr = ref(false); // 할 일 적지 않았을 경우 체크 플래그
 
+    // 할 일 추가
     const onSubmit = () => {
-      // console.log('onSubmit !!');
+      if(todo.value === '') {
+        hasErr.value = true;
+      }
+      else {
+        todoList.value.push({
+          id: Date.now(),
+          subject: todo.value,
+          completed: false
+        })
+        hasErr.value = false;
+        todo.value = '';
+      }
+      
+    };
 
-      // add task 
-      todoList.value.push({
-        id: Date.now(),
-        subject: todo.value
-      })
+    // 할 일 삭제
+    const deleteTask = (index) => {
+      todoList.value.splice(index, 1);
     };
 
     return {
       todo,
       todoList,
-      onSubmit
+      hasErr,
+      onSubmit,
+      deleteTask
     }
   }
 }
 </script>
 
 <style>
-
+  .todoCompleted { color: gray; text-decoration: line-through; }
 </style>
