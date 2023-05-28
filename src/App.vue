9f3e00c0191bff900a20f6c0be37c7ef2859a931
KSY <yoonion97@gmail.com>
2023-05-28 17:02:00 +0900

    Refactor: 할 일 추가 폼 컴포넌트화

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,7 @@
   <div class="container">
 
     <h1>To-Do List</h1>
-    <form @submit.prevent="onSubmit">
-      <div class="d-flex">
-        <div class="flex-grow-1 mr-2">
-          <input class="form-control" type="text" v-model="todo" placeholder="Write new task">
-        </div>
-        <div>
-          <button class="btn btn-primary" type="submit">Add</button>  
-        </div>
-      </div>
-      <div v-show="hasErr" style="color: red">This field cannot be empty</div>
-    </form>
-
+    <TodoSimpleForm @add-task="addTask" />
     <div v-for="(todo, index) in todoList" :key="todo.id" class="card mt-2">
       <div class="card-body p-2 d-flex align-items-center">
         <div class="form-check flex-grow-1">
@@ -31,28 +20,19 @@
 
 <script>
 import { ref } from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
 
 export default {
+  components: {
+    TodoSimpleForm
+  },
   setup() {
-    const todo = ref('');
+    
     const todoList = ref([]);
-    const hasErr = ref(false); // 할 일 적지 않았을 경우 체크 플래그
 
-    // 할 일 추가
-    const onSubmit = () => {
-      if(todo.value === '') {
-        hasErr.value = true;
-      }
-      else {
-        todoList.value.push({
-          id: Date.now(),
-          subject: todo.value,
-          completed: false
-        })
-        hasErr.value = false;
-        todo.value = '';
-      }
-      
+    // TodoSimpleForm 컴포넌트에서 받아온 데이터 추가 시킨다
+    const addTask = (todo) => {
+      todoList.value.push(todo);
     };
 
     // 할 일 삭제
@@ -61,10 +41,8 @@ export default {
     };
 
     return {
-      todo,
       todoList,
-      hasErr,
-      onSubmit,
+      addTask,
       deleteTask
     }
   }
---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input class="form-control" type="text" v-model="todo" placeholder="Write new task">
+        </div>
+        <div>
+          <button class="btn btn-primary" type="submit">Add</button>  
+        </div>
+      </div>
+      <div v-show="hasErr" style="color: red">This field cannot be empty</div>
+    </form>
+</template>
+
+<script>
+  import { ref } from 'vue';
+
+  export default {
+    setup(porps, context) {
+      const todo = ref('');
+      const hasErr = ref(false); // 할 일 적지 않았을 경우 체크 플래그
+
+      // 할 일 추가
+      const onSubmit = () => {
+        if(todo.value === '') {
+          hasErr.value = true;
+        }
+        else {
+          context.emit('add-task', {
+            id: Date.now(),
+            subject: todo.value,
+            completed: false  
+          });
+          hasErr.value = false;
+          todo.value = '';
+        }
+      };
+
+      return {
+        todo,
+        hasErr,
+        onSubmit
+      }
+    }
+  }
+</script>
+
+<style></style>
