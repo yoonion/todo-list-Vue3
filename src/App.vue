ab2a174e044af43af0f8bda3d5befe63d7ecc86c
KSY <yoonion97@gmail.com>
2023-05-29 17:01:21 +0900

    Chore: 할 일 기존 배열 사용에서 Json Server 로컬 개발용 데이터 베이스로 변경

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
     <input class="form-control" type="text" v-model="searchText" placeholder="Search">
     <hr>
     <TodoSimpleForm @add-task="addTask" />
-    <div v-if="!filteredTodos.length">todo가 없습니다.</div>
+    <div style="color: red">{{ error }}</div>
+    <div v-if="!filteredTodos.length">할 일이 없습니다.</div>
     <TodoList :todoList="filteredTodos" @toggle-todo="toggleTodo" @delete-task="deleteTask" />
 
   </div>
@@ -15,6 +16,7 @@
 import { ref, computed } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -24,22 +26,66 @@ export default {
   setup() {
     
     const todoList = ref([]);
+    const error = ref('');
+
+    // json-server todoList 가져오기
+    const getTodoList = async () => {
+      try {
+        const res = await axios.get('http://localhost:3000/todoList');
+        todoList.value = res.data; // 가져온 todo데이터를 넣어준다.
+      }
+      catch (err) {
+        console.log(err);
+        error.value = '!! ERROR: getTodoList error. !!'
+      }
+    }
+    getTodoList();
 
     // 할 일 추가 - TodoSimpleForm 컴포넌트에서 받아온 데이터 추가 시킨다
-    const addTask = (todo) => {
-      todoList.value.push(todo);
+    const addTask = async (todo) => {
+      // 데이터베이스(json-server)에 저장
+      try {
+        const res = await axios.post('http://localhost:3000/todoList', {
+          subject: todo.subject,
+          completed: todo.completed
+      });
+      todoList.value.push(res.data);
+      }
+      catch (err) {
+        console.log(err);
+        error.value = '!! ERROR: addTask error. !!'
+      }
     };
 
     // 할 일 삭제
-    const deleteTask = (index) => {
-      todoList.value.splice(index, 1);
+    const deleteTask = async (index) => {
+      const id = todoList.value[index].id; // 할 일 id
+      try {
+        await axios.delete('http://localhost:3000/todoList/' + id);
+        todoList.value.splice(index, 1);
+      }
+      catch (err) {
+        console.log(err);
+        error.value = '!! ERROR: deleteTask error. !!'
+      }
     };
 
     // 완료한 일 체크 - TodoList 컴포넌트에서 받아온 데이터로 수정한다. > 자식 컴포넌트 자체에서 데이터 조작하면 안 됨
-    const toggleTodo = (index) => {
-      todoList.value[index].completed = !todoList.value[index].completed;
+    const toggleTodo = async (index) => {
+      const id = todoList.value[index].id; // 할 일 id
+      try {
+        await axios.patch('http://localhost:3000/todoList/' + id, {
+          completed: !todoList.value[index].completed
+        });
+        todoList.value[index].completed = !todoList.value[index].completed;
+      }
+      catch (err) {
+        console.log(err);
+        error.value = '!! ERROR: toggleTodo error. !!'
+      }
     }
 
+    // todo 검색
     const searchText = ref('');
     const filteredTodos = computed(() => {
       if(searchText.value) {
@@ -57,7 +103,9 @@ export default {
       deleteTask,
       toggleTodo,
       searchText,
-      filteredTodos
+      filteredTodos,
+      error,
+      getTodoList
     }
   }
 }
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,15 @@
 <template>
   <div v-for="(todo, index) in todoList" :key="todo.id" class="card mt-2">
-      <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-          <input :value="todo.completed" @change="toggleTodo(index)" class="form-check-input" type="checkbox">
-          <label :class="{ todoCompleted: todo.completed }" class="form-check-label">{{ todo.subject }}</label>
-        </div>
-        <div>
-            <button class="btn btn-danger btn-sm" @click="deleteTask(index)">Delete</button>
-          </div>
+    <div class="card-body p-2 d-flex align-items-center">
+      <div class="form-check flex-grow-1">
+        <input :checked="todo.completed" @change="toggleTodo(index)" class="form-check-input" type="checkbox">
+        <label :class="{ todoCompleted: todo.completed }" class="form-check-label">{{ todo.subject }}</label>
+      </div>
+      <div>
+        <button class="btn btn-danger btn-sm" @click="deleteTask(index)">Delete</button>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
